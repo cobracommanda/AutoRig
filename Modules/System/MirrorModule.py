f956c9638b95798b54c0ef7fa6f2155dd1fdf72c
DRobinson <drobinson@benchmarkeducation.com>
2024-05-22 18:24:36 -0400

    mirroring-ui-pt-4

--- Modules/System/MirrorModule.py
+++ Modules/System/MirrorModule.py
@@ -76,4 +76,63 @@ class MirrorModule(QtWidgets.QDialog):
         return module_inst.can_module_be_mirrored()
     
     def mirror_module_UI(self):
-        print(self.modules)
+        self.module_names = []
+        for module in self.modules:
+            self.module_names.append(module.partition("__")[2])
+        self.same_mirror_settings_for_all = False
+        
+        if len(self.modules) > 1:
+            result = cmds.confirmDialog(title="Mirror Multilple Module", message=f"{str(len(self.modules))} modules selected for mirror.\nHow would you like to apply mirror settings?", b=["Same for all", "Individually", "Cancel"], db= "Same for all", cb="Cancel", ds="Cancel")
+            
+            if result == "Cancel":
+                return
+            elif result == "Same for all":
+                self.same_mirror_settings_for_all = True
+        
+        self.UI_elements = {}
+        
+        if cmds.window("mirror_module_UI_window", exists=True):
+            cmds.deleteUI("mirror_module_UI_window")
+        
+        window_width = 300
+        window_height = 400
+        scroll_width = window_width - 30
+
+        mirror_plane_text_width = 80
+        mirror_plane_radio_width = (scroll_width - mirror_plane_text_width) / 3
+
+        self.UI_elements['window'] = cmds.window("mirror_module_UI_window", w=window_width, h=window_height, t="Mirror Module(s)", s=0)
+        self.UI_elements['scroll_layout'] = cmds.scrollLayout(hst=0)
+        self.UI_elements['top_column_layout'] = cmds.columnLayout(adj=1, rs=3)
+
+        self.UI_elements['mirror_plane_row_column'] = cmds.rowColumnLayout(
+            nc=4, 
+            cat=[(1, "right", 0), (2, "both", 0), (3, "both", 0), (4, "both", 0)], 
+            cw=[
+                (1, mirror_plane_text_width), 
+                (2, mirror_plane_radio_width), 
+                (3, mirror_plane_radio_width), 
+                (4, mirror_plane_radio_width)
+            ]
+        )
+
+        cmds.text(l="Mirror Plane: ")
+        self.UI_elements["mirror_plane_radio_collection"] = cmds.radioCollection()
+        cmds.radioButton("XY", l="XY", sl=0)
+        cmds.radioButton("YZ", l="YZ", sl=1)
+        cmds.radioButton("XZ", l="XZ", sl=0)
+
+        cmds.setParent(self.UI_elements['top_column_layout'])
+        cmds.separator()
+        cmds.text(l="Mirrored Name (s):")
+        
+        column_width = scroll_width / 2
+        self.UI_elements['module_name_row_column'] = cmds.rowColumnLayout(nc=2, cat=(1, "right", 0) ,cw=[(1, column_width), (2, column_width)])
+        for module in self.module_names:
+            cmds.text(l=f"{module} >> ")
+            self.UI_elements["moduleName_"+module] = cmds.textField(en=1, tx=f"{module}_mirror")
+        
+        
+        cmds.showWindow(self.UI_elements['window'])
+        
+        
